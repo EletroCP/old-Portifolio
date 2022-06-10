5c41e8eef4f371eb3cd0b5799b2708e60ff376a6
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-09 21:05:41 -0300

    feat: testes da sessão hero implementados

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,6 @@
 describe('Testes de responsividade e funcionalidade do portifolio', () => {
-  const sizes = ['samsung-s10', 'ipad-2', 'macbook-13']
+  const sizes = ['samsung-s10', 'ipad-2', 'macbook-13'];
+  const heroImage = '';
 
   context('Testes de responsividade e funcionalidade mobile', () => {
     beforeEach(() => {
@@ -9,31 +10,46 @@ describe('Testes de responsividade e funcionalidade do portifolio', () => {
 
     it('Existe um componente header', () => {
       cy.get('header')
-            .should('exist')
+            .should('exist');
     });
     
     it('Está escrito portifólio no header', () => {
     	cy.get('#title-header')
         	.should('exist')
 		cy.get('#title-header')
-			.should('have.text', 'Portifólio')
+			  .should('have.text', 'Portifólio');
     });
 
 	it('O botão do menu hamburger está presente', () => {
 		cy.get('#hamburger')
-			.should('exist')
+			  .should('exist');
 	});
 
   it('Inicialmente a navegação não é visivel', () => {
     cy.get('#menu')
-        .should('be.hidden')
+        .should('be.hidden');
   })
 
   it('Ao clicar no botão hamburger, o menu de navegação aparece', () => {
     cy.get('#hamburger')
         .click()
     cy.get('#menu')
-        .should('be.visible')
+        .should('be.visible');
+  });
+
+  it('Na sessão "hero" o elemento image possui o src correto', () => {
+    cy.get('#hero-image')
+        .should('have.attr', 'src', './images/heroImage.png');
+  });
+
+  it('O "title" da sessão hero é "Breno Laskavski Lopes"', () => {
+    cy.get('#hero-title')
+        .should('have.text', 'Breno Laskavski Lopes');
+  });
+
+  it('O "about me" possui no minimo 100 caracteres', () => {
+    cy.get('#hero-describe')
+        .should('have.length', 100);
   });
   });
 
