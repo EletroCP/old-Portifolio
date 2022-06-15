ef67ea0a6e9633566bc4ce757cc71bc947a0940c
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-15 18:43:29 -0300

    fix: carousel editado para ter descrição sob imagens

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const hamburgetBtn = document.getElementById('hamburger');
 const imagesCarousel = document.querySelectorAll('.development-image');
-const carrousel = document.querySelector('#carousel')
+
+const carrousel = document.querySelector('.carousel')
     .addEventListener('wheel', (event) => {
         if(event.deltaY > 0) {
-            event.target.scrollBy(300, 0)
+            event.target.scrollBy(300, 0);
         }
-        event.target.scrollBy(-300,0)
-    })
+        event.target.scrollBy(-300,0);
+    });
 
 const openMenu = () => {
     const menuHamburger = document.getElementById('menu');
-    menuHamburger.classList.toggle('exist')
+    menuHamburger.classList.toggle('exist');
 }
 
 hamburgetBtn.addEventListener('click', openMenu);
