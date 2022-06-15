dceffffb9600a5654e27dff3b1160b691165aac4
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-15 20:11:44 -0300

    feat: adiciodos botões para trocar as imagens do caroseul

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const hamburgetBtn = document.getElementById('hamburger');
 const imagesCarousel = document.querySelectorAll('.development-image');
 
-const carrousel = document.querySelector('.carousel')
-    .addEventListener('wheel', (event) => {
+const carrousel = document.querySelector('.carousel');
+    carrousel.addEventListener('wheel', (event) => {
         if(event.deltaY > 0) {
             event.target.scrollBy(300, 0);
         }
         event.target.scrollBy(-300,0);
+        console.log(event.target)
     });
 
 const openMenu = () => {
@@ -15,3 +16,15 @@ const openMenu = () => {
 }
 
 hamburgetBtn.addEventListener('click', openMenu);
+
+const leftArrow = document.querySelector('#left-arrow');
+const rigthArrow = document.querySelector('#rigth-arrow');
+
+leftArrow.addEventListener('click', () => {
+    console.log(carrousel)
+    carrousel.scrollBy(-300, 0)
+})
+
+rigthArrow.addEventListener('click', () => {
+    carrousel.scrollBy(300, 0)
+})
