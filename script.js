37878cdbea2bb4f782e5b081799495397a1e7ed5
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-19 16:36:43 -0300

    feat: adicionado style para descktop

--- script.js
+++ script.js
@@ -18,11 +18,21 @@ hamburgetBtn.addEventListener('click', openMenu);
 
 const leftArrow = document.querySelector('#left-arrow');
 const rigthArrow = document.querySelector('#rigth-arrow');
+const leftArrow2 = document.querySelector('#left-arrow-fullscream');
+const rigthArrow2 = document.querySelector('#rigth-arrow-fullscream');
 
 leftArrow.addEventListener('click', () => {
     carrousel.scrollBy(-300, 0)
 })
 
+leftArrow2.addEventListener('click', () => {
+    carrousel.scrollBy(-300, 0)
+})
+
 rigthArrow.addEventListener('click', () => {
+    carrousel.scrollBy(300, 0)
+})
+
+rigthArrow2.addEventListener('click', () => {
     carrousel.scrollBy(300, 0)
 })
