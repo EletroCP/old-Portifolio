9cf803bf1096429066c5ca7d14b30c9596667844
Breno L. Lopes <brernolopes@hotmail.com>
2022-06-26 21:27:38 -0300

    feat: Botões de mover o caroseul de projetos funcionando

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const data = [
+    {
+      link: 'https://eletrocp.github.io/Pixel-Palette/',
+      thumb: './src/images/PixelPalette.png',
+      describe: 'Projeto simples de desenho em pixel art.',
+      name: 'Pixel Palette'
+    },
+  ];
+
 const hamburgetBtn = document.getElementById('hamburger');
 const imagesCarousel = document.querySelectorAll('.development-image');
-
 const carrousel = document.querySelector('.carousel');
+
 carrousel.addEventListener('wheel', (event) => {
     if (event.deltaY > 0) {
         event.target.scrollBy(300, 0);
@@ -18,8 +27,12 @@ hamburgetBtn.addEventListener('click', openMenu);
 
 const leftArrow = document.querySelector('#left-arrow');
 const rigthArrow = document.querySelector('#rigth-arrow');
-const leftArrow2 = document.querySelector('#left-arrow-fullscream');
-const rigthArrow2 = document.querySelector('#rigth-arrow-fullscream');
+const leftArrow2 = document.querySelector('.left-arrow-fullscream');
+const rigthArrow2 = document.querySelector('.rigth-arrow-fullscream');
+const projectContainer = document.getElementById('project-container');
+const containerProject = document.getElementById('container-projects');
+const leftArrowProject = document.getElementById('arrowProjectLeft');
+const rigthArrowProject = document.getElementById('arrowProjectRigth');
 
 leftArrow.addEventListener('click', () => {
     carrousel.scrollBy(-300, 0)
@@ -36,3 +49,51 @@ rigthArrow.addEventListener('click', () => {
 rigthArrow2.addEventListener('click', () => {
     carrousel.scrollBy(300, 0)
 })
+
+leftArrowProject.addEventListener('click', () => {
+    console.log('oi')
+    projectContainer.scrollBy(-300, 0)
+})
+
+rigthArrowProject.addEventListener('click', () => {
+    console.log('oi')
+
+    projectContainer.scrollBy(300, 0)
+})
+
+const project = () => {
+    for (let index = 0; index < data.length; index +=1) {
+        const { link, thumb, describe, name } = data[index];
+        const div = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const textContaier = document.createElement('div');
+        const img = document.createElement('img');
+
+        const p = document.createElement('p');
+        const a = document.createElement('a');
+    
+        div.classList.add('block-caroseul');
+        a.setAttribute('href', link);
+        a.setAttribute('target', '_blank')
+        a.id = 'linkProject';
+        img.setAttribute('src', thumb);
+        img.setAttribute('alt', describe);
+
+        img.classList.add('development-image');
+        h3.innerText = name;
+        p.innerText = describe;
+        a.style.width = '100%';
+        textContaier.classList.add('textContainer');
+    
+        projectContainer.appendChild(div);
+        div.appendChild(textContaier);
+        textContaier.appendChild(a);
+        a.appendChild(h3);
+        div.appendChild(img);
+        textContaier.appendChild(p);
+    }
+    };
+
+window.onload = () => {
+    project()
+}
