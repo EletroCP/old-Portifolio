afecb70043f1746727fd540c0098b67b1f89c2ea
Breno L. Lopes <brernolopes@hotmail.com>
2022-08-16 15:39:36 -0300

    feat: adicionado projeto "pick a color"

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    link: 'https://eletrocp.github.io/Color-Guess/',
+    thumb: './src/images/pickAColor.png',
+    describe: 'Um simples porem divertido jogo de adivinhação de cores.',
+    name: 'Pick a Color',
+  },
+  {
     link: 'https://eletrocp.github.io/Pixel-Palette/',
     thumb: './src/images/PixelPalette.png',
     describe: 'Projeto simples de desenho em pixel art.',
@@ -12,15 +18,15 @@ const projectsData = [
     name: 'To do List'
   },
   {
+    link: 'https://eletrocp.github.io/Meme-Generator/',
+    thumb: './src/images/meme-generator.png',
+    describe: 'Simples criador/editor de memes.',
+    name: 'Meme Generator'
+  },
+  {
     link: 'https://eletrocp.github.io/Mistery-Letter/',
     thumb: './src/images/project-mistery-letter.png',
     describe: 'Gerador de "Mensagem misteriosa"',
     name: 'Mistery Letter'
   },
-  {
-    link: 'https://eletrocp.github.io/Meme-Generator/',
-    thumb: './src/images/meme-generator.png',
-    describe: 'Simples criador/editor de memes.',
-    name: 'Meme Generator'
-  }
 ];
